c40c352d5ed15b96c231f2062c317fee524695fe
MeLonMzfk54 <garifullin.antoshka@mail.ru>
2021-07-28 23:09:26 +0700

    скелет формы, второй блок

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
+
+    
     $(".header__burger").on('click',function(){
         $(this).toggleClass("active");
         $(".header__menu").toggleClass("active");
     });
+
+    $(".order__wrapper").on('click',function(){
+        $(".order__arrow").toggleClass("active");
+        $(this).toggleClass("active");
+    });
+
+    $("#sel").on("focusout",function(){
+        $("#sel").removeAttr("size");
+        $("#sel").css("padding-top","0px");
+        $(".order__arrow").removeClass("active");
+        $('.order__wrapper').removeClass("active");
+    });
+
 });
