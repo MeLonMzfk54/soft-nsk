eb1840169e272086c8829eb4e24f4a73c32fa42e
MeLonMzfk54 <garifullin.antoshka@mail.ru>
2021-07-29 19:58:07 +0700

    V 1.0
    Готовый сайт
    P.S Проверить еще раз функционал, адаптив, доделать селект

--- js/main.js
+++ js/main.js
@@ -1,21 +1,30 @@
 $(document).ready(function(){
 
-    
     $(".header__burger").on('click',function(){
         $(this).toggleClass("active");
         $(".header__menu").toggleClass("active");
     });
 
-    $("#sel").on('click',function(){
+    $("#sel").on('click',function(){              
         $(".order__arrow").toggleClass("active");
         $(this).toggleClass("active");
     });
 
     $("#sel").on("focusout",function(){
-        $(this).removeAttr("size");
-        $(this).css("padding-top","0px");
         $(".order__arrow").removeClass("active");
         $(this).removeClass("active");
     });
 
+    $(".scroll__link").on('click',function(e){
+        e.preventDefault();
+        let top = $($(this).attr("data-scroll")).offset().top;
+        $(".header__burger").removeClass("active");
+        $(".header__menu").removeClass('active');
+        $("html, body").animate({scrollTop: top}, 500);
+    });
+
+    $('.order__res').text($(".order__range").val() + " %");
+    $(".order__range").on("input change",function(){
+        $(".order__res").html($(this).val() + " %");
+    });
 });
